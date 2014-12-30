0068ae173db261727d67777ae7995c176b1f1d7b
jmorrill <jeremiah.morrill@econnect.tv>
2014-12-29 18:05:07 -0800

    filestream work

--- libuvxx/include/fs/file_stream.hpp
+++ libuvxx/include/fs/file_stream.hpp
@@ -134,11 +134,10 @@ namespace uvxx { namespace fs {
             }
             else
             {
-                return (m_info->m_buffer_read_length - m_info->m_buffer_read_pos) / sizeof(_CharType);
+                return static_cast<size_t>(m_info->m_buffer_read_length - m_info->m_buffer_read_pos) / sizeof(_CharType);
             }
         }
         
-        // Use a separated function for working around Dev10's ICE
         pplx::task<void> _close_read_impl()
         {
             uvxx::streams::details::streambuf_state_manager<_CharType>::_close_read();
@@ -221,7 +220,7 @@ namespace uvxx { namespace fs {
 
                     return static_cast<int_type>(ch);
                 }
-                catch (...)
+                catch (uvxx_exception_with_code const&)
                 {
                     return static_cast<int_type>(traits::eof());
                 }
@@ -339,7 +338,7 @@ namespace uvxx { namespace fs {
 
             bool use_buffer = false;
 
-            total_user_bytes = m_info->m_buffer_write_pos + user_bytes;
+            total_user_bytes = static_cast<size_t>(m_info->m_buffer_write_pos + user_bytes);
 
             use_buffer = (total_user_bytes + user_bytes < (size_t)(m_info->m_memory_buffer.length_get() * 2));
             
@@ -367,10 +366,12 @@ namespace uvxx { namespace fs {
 
                 return task_chain.then([=](size_t)
                 {
+                    auto write_position = m_info->m_file_write_pos == -1 ? -1 : m_info->m_file_write_pos * sizeof(_CharType);
+
                     return m_file.write_async(m_info->m_memory_buffer,
-                                       0,
-                                       m_info->m_buffer_write_pos,
-                                       m_info->m_file_write_pos * sizeof(_CharType));
+                                       static_cast<size_t>(0),
+                                       static_cast<size_t>(m_info->m_buffer_write_pos),
+                                       write_position);
                 }).
                 then([=](size_t bytes_wrote) mutable
                 {
@@ -383,14 +384,15 @@ namespace uvxx { namespace fs {
             }
             else
             {
-
                 if (m_info->m_buffer_write_pos > 0)
                 {
                     task_chain.then([=](size_t)
                     {
+                        auto write_position = m_info->m_file_write_pos == -1 ? -1 : m_info->m_file_write_pos * sizeof(_CharType);
+
                         return m_file.write_async(m_info->m_memory_buffer,
-                                                  0, 
-                                                  m_info->m_buffer_write_pos * sizeof(_CharType));
+                                                  static_cast<size_t>(0), 
+                                                  static_cast<size_t>(m_info->m_buffer_write_pos) * sizeof(_CharType));
                     }).
                     then([=](uvxx::pplx::task<size_t> t)
                     {
@@ -398,7 +400,10 @@ namespace uvxx { namespace fs {
 
                         m_info->m_buffer_write_pos = 0;
 
-                        m_info->m_file_write_pos += (bytes_wrote / sizeof(_CharType));
+                        if (m_info->m_file_write_pos != -1)
+                        {
+                            m_info->m_file_write_pos += (bytes_wrote / sizeof(_CharType));
+                        }
 
                         return bytes_wrote;
                     });
@@ -406,10 +411,13 @@ namespace uvxx { namespace fs {
 
                 return task_chain.then([=](size_t bytes) mutable
                 {
+                     auto write_position = m_info->m_file_write_pos == -1 ? -1 : m_info->m_file_write_pos * sizeof(_CharType);
+
                      return m_file.write_async(reinterpret_cast<const uint8_t*>(const_cast<_CharType*>(ptr)), 
                                                m_info->m_memory_buffer.length_get(),
-                                               0, 
-                                               user_bytes, m_info->m_file_write_pos * sizeof(_CharType));
+                                               static_cast<size_t>(0), 
+                                               user_bytes, 
+                                               write_position);
                 }).
                 then([=](pplx::task<size_t> t)
                 {
@@ -530,7 +538,7 @@ namespace uvxx { namespace fs {
 
                     return static_cast<int_type>(result);
                 }
-                catch (...)
+                catch (uvxx_exception_with_code const&)
                 {
                     return static_cast<int_type>(traits::eof());
                 }
@@ -556,31 +564,80 @@ namespace uvxx { namespace fs {
         /// <summary>
         /// Advances the read position, then return the next character withouth advancing again.
         /// </summary>
-            /// <returns>A <c>task</c> that holds the value of the byte, which is EOF if the read fails.</returns>
+        /// <returns>A <c>task</c> that holds the value of the byte, which is EOF if the read fails.</returns>
         virtual pplx::task<int_type> _nextc()
         {
-            return create_task([this]()
+            auto char_buff = std::make_shared<int_type>(traits::eof());
+
+            this->seekoff(1, std::ios_base::cur, std::ios_base::in);
+
+            return _getn(static_cast<_CharType*>(static_cast<void*>(char_buff.get())), 1)
+            .then([=](pplx::task<size_t> t)
             {
-                //_seekrdpos_fsb(m_info, m_info->m_rdpos+1, sizeof(_CharType));
-                //if ( m_info->m_atend )
-                //    return pplx::task_from_result(basic_file_buffer<_CharType>::traits::eof());
-                return static_cast<int_type>(0);//this->_getcImpl();
+                size_t bytes_read = 0;
+
+                if (m_info->m_at_end)
+                {
+                    return traits::eof();
+                }
+
+                try
+                {
+                    bytes_read = t.get();
+
+                    if (bytes_read == 0 || bytes_read == traits::eof())
+                    {
+                        return traits::eof();
+                    }
+
+                    seekoff(-1, std::ios_base::cur, std::ios_base::in);
+
+                    return *char_buff.get();
+                }
+                catch (uvxx_exception_with_code const&)
+                {
+                	return traits::eof();
+                }
             });
         }
 
         /// <summary>
         /// Retreats the read position, then return the current character withouth advancing.
         /// </summary>
-            /// <returns>A <c>task</c> that holds the value of the byte. The value is EOF if the read fails, <c>requires_async</c> if an asynchronous read is required</returns>
+        /// <returns>A <c>task</c> that holds the value of the byte. The value is EOF if the read fails, <c>requires_async</c> if an asynchronous read is required</returns>
         virtual pplx::task<int_type> _ungetc() 
         {
-            return create_task([this]() 
+            auto char_buff = std::make_shared<int_type>(traits::eof());
+
+            seekoff(-1, std::ios_base::cur, std::ios_base::in);
+
+            return _getn(static_cast<_CharType*>(static_cast<void*>(char_buff.get())), 1)
+            .then([=](pplx::task<size_t> t)
             {
-               /* if ( m_info->m_rdpos == 0 ) 
-                    return pplx::task_from_result<int_type>(basic_file_buffer<_CharType>::traits::eof());
-                _seekrdpos_fsb(m_info, m_info->m_rdpos-1, sizeof(_CharType));
-                */
-                return static_cast<int_type>(0);//this->_getcImpl();
+                size_t bytes_read = 0;
+
+                if (m_info->m_at_end)
+                {
+                    return traits::eof();
+                }
+
+                try
+                {
+                    bytes_read = t.get();
+
+                    if (bytes_read == 0 || bytes_read == traits::eof())
+                    {
+                        return traits::eof();
+                    }
+
+                    seekoff(-1, std::ios_base::cur, std::ios_base::in);
+
+                    return *char_buff.get();
+                }
+                catch (uvxx_exception_with_code const&)
+                {
+                	return traits::eof();
+                }
             });
         }
 
@@ -609,19 +666,23 @@ namespace uvxx { namespace fs {
         {
             auto current_position = m_info->m_file_flush_pos == -1 ? m_info->m_file_write_pos : m_info->m_file_flush_pos;
             
+            auto write_position = current_position == -1 ? -1 : current_position * sizeof(_CharType);
+
             return m_file.write_async(m_info->m_memory_buffer, 
                                       0, 
-                                      m_info->m_buffer_write_pos, 
-                                      current_position * sizeof(_CharType)).
+                                      static_cast<size_t>(m_info->m_buffer_write_pos), 
+                                      current_position * static_cast<int64_t>(sizeof(_CharType))).
             then([=](uvxx::pplx::task<size_t> t)
             {
                 auto bytes_wrote = t.get();
 
-                if (m_info->m_file_flush_pos != -1)
+                if (m_info->m_file_write_pos != -1 && m_info->m_file_flush_pos != -1)
+                {
+                    m_info->m_file_write_pos += (bytes_wrote / sizeof(_CharType));
+                }
+                else if (m_info->m_file_flush_pos != -1)
                 {
                     m_info->m_file_flush_pos = -1;
-
-                    m_info->m_file_write_pos += (bytes_wrote / sizeof(_CharType));
                 }
 
                 return bytes_wrote;
@@ -697,7 +758,7 @@ namespace uvxx { namespace fs {
                 {
                     bytes_read = t.get();
                 }
-                catch (...)
+                catch (uv_exception_with_code const&)
                 {
                     return static_cast<size_t>(traits::eof());
                 }
@@ -721,21 +782,15 @@ namespace uvxx { namespace fs {
         /// <remarks>This is a synchronous operation, but is guaranteed to never block.</remarks>
         size_t _sgetn(_Out_writes_ (count) _CharType *ptr, _In_ size_t count)
         {
-           /* m_readOps.wait();*/
-            /*if ( m_info->m_at_end ) return 0;
-
             if ( count == 0 || in_avail() == 0 ) return 0;
 
-            size_t available = _in_avail_unprot();
-            size_t copy = (count < available) ? count : available;
+            auto user_count = count * sizeof(_CharType);
 
-            auto bufoff = m_info->m_read_pos - m_info->m_bufoff;
-            std::memcpy((void *)ptr, this->m_info->m_buffer+bufoff*sizeof(_CharType), copy*sizeof(_CharType));
-            
-            m_info->m_read_pos += copy;
-            m_info->m_at_end = (copy < count);
-            return copy;*/
-            return 0;
+            auto bytes_read = read_from_buffer(ptr, user_count);
+
+            m_info->m_at_end = (bytes_read < user_count);
+
+            return bytes_read;
         }
 
         /// <summary>
@@ -817,17 +872,15 @@ namespace uvxx { namespace fs {
         {
             if ( mode == std::ios_base::in ) 
             {
+                int64_t current_pos = m_info->m_file_read_pos - (m_info->m_buffer_read_length - m_info->m_buffer_read_pos);
+
                 switch ( way )
                 {
                 case std::ios_base::beg:
-                    m_info->m_buffer_read_pos = m_info->m_buffer_read_length = 0;
-
-                    return m_info->m_file_read_pos = offset;
+                    return seekpos(offset, mode);
 
                 case std::ios_base::cur:
-                    m_info->m_buffer_read_pos = m_info->m_buffer_read_length = 0;
-
-                    return m_info->m_file_read_pos = m_info->m_file_read_pos + offset;
+                    return seekpos(current_pos + offset, mode);
 
                 case std::ios_base::end:
                    // return (pos_type)_seekrdtoend_fsb(m_info, int64_t(offset), sizeof(_CharType));
@@ -839,21 +892,14 @@ namespace uvxx { namespace fs {
                 switch ( way )
                 {
                 case std::ios_base::beg:
-                    if (m_info->m_buffer_write_pos > 0 && m_info->m_file_flush_pos == -1)
-                    {
-                        m_info->m_file_flush_pos = m_info->m_file_write_pos;
-                    }
+                    return seekpos(offset, mode);
 
-                    return m_info->m_file_write_pos = offset;
                 case std::ios_base::cur:
-                    if (m_info->m_buffer_write_pos > 0 && m_info->m_file_flush_pos == -1)
-                    {
-                        m_info->m_file_flush_pos = m_info->m_file_write_pos;
-                    }
+                    return seekpos(offset + m_info->m_file_write_pos, mode);
 
-                    return m_info->m_file_write_pos += offset;
                 case std::ios_base::end:
-                    //return (pos_type)_seekwrpos_fsb(m_info, size_t(-1), sizeof(_CharType));
+                    return seekpos(size_t(-1), mode);
+
                     break;
                 }
             }
@@ -897,15 +943,16 @@ namespace uvxx { namespace fs {
             return task_chain.then([](size_t){});
         }
 
-        basic_file_buffer(std::unique_ptr<_file_info> info, file _file) : m_info(std::move(info)), m_file(_file), uvxx::streams::details::streambuf_state_manager<_CharType>(info->m_mode)
+        basic_file_buffer(std::unique_ptr<_file_info> info, file _file) : m_info(std::move(info)), 
+                                                                          m_file(_file), 
+                                                                          uvxx::streams::details::streambuf_state_manager<_CharType>(info->m_mode)
         { 
 
         }
 
         static pplx::task<std::shared_ptr<uvxx::streams::details::basic_streambuf<_CharType>>> open(
             const utility::string_t &_Filename,
-            std::ios_base::openmode _Mode = std::ios_base::out,
-            int _Prot = 0 /* unsupported on Linux, for now */ )
+            std::ios_base::openmode _Mode = std::ios_base::out)
         {
              uvxx::fs::file _file;
 
@@ -913,6 +960,7 @@ namespace uvxx { namespace fs {
             then([=](uvxx::pplx::task<void> t)
             {
                 t.get();
+
                 return uvxx::fs::file::get_file_info_async(_Filename);
             }).
             then([=](uvxx::pplx::task<file_info> t) mutable -> std::shared_ptr<uvxx::streams::details::basic_streambuf<_CharType>>
@@ -922,21 +970,26 @@ namespace uvxx { namespace fs {
                 {
                     fileinfo = t.get();
                 }
-                catch (...)
+                catch (uv_exception_with_code const&)
                 {
-                	
+                	throw;
                 }
 
                 auto info = std::unique_ptr<_file_info>(new _file_info(_Mode, 1024 * 1024));
 
                 info->m_file_size = fileinfo.length_get();
 
+                if ( _Mode & std::ios_base::app || _Mode & std::ios_base::ate )
+                {
+                    info->m_file_write_pos = static_cast<size_t>(-1); // Start at the end of the file.
+                }
+
                 auto buff = new basic_file_buffer<_CharType>(std::move(info), _file);
+
                 auto file_buff = std::shared_ptr<basic_file_buffer<_CharType>>(buff);
 
-                return std::dynamic_pointer_cast<uvxx::streams::details::basic_streambuf<_CharType>>(file_buff);
+                return std::static_pointer_cast<uvxx::streams::details::basic_streambuf<_CharType>>(file_buff);
             });
-
         }
 
         uvxx::fs::file m_file;
@@ -964,11 +1017,10 @@ namespace uvxx { namespace fs {
         /// <returns>A <c>task</c> that returns an opened stream buffer on completion.</returns>
         static pplx::task<uvxx::streams::streambuf<_CharType>> open(
             const utility::string_t &file_name,
-            std::ios_base::openmode mode = std::ios_base::out,
-            int prot = 0 // unsupported on Linux
+            std::ios_base::openmode mode = std::ios_base::out
             )
         {
-            auto bfb = basic_file_buffer<_CharType>::open(file_name, mode, prot);
+            auto bfb = basic_file_buffer<_CharType>::open(file_name, mode);
             return bfb.then([](pplx::task<std::shared_ptr<uvxx::streams::details::basic_streambuf<_CharType>>> op)  mutable -> uvxx::streams::streambuf<_CharType>
                             {
                                 return uvxx::streams::streambuf<_CharType>(op.get());
@@ -998,13 +1050,12 @@ namespace uvxx { namespace fs {
         /// <returns>A <c>task</c> that returns an opened input stream on completion.</returns>
         static pplx::task<streams::basic_istream<_CharType>> open_istream(
             const utility::string_t &file_name,
-            std::ios_base::openmode mode = std::ios_base::in,
-            int prot = 0
+            std::ios_base::openmode mode = std::ios_base::in
             )
         {
             mode |= std::ios_base::in;
-            return file_buffer<_CharType>::open(file_name, mode, prot)
-                .then([](streams::streambuf<_CharType> buf) -> uvxx::streams::basic_istream < _CharType >
+            return file_buffer<_CharType>::open(file_name, mode)
+                .then([](streams::streambuf<_CharType> buf) -> uvxx::streams::basic_istream<_CharType>
             {
                 return uvxx::streams::basic_istream<_CharType>(buf);
             });
@@ -1027,7 +1078,7 @@ namespace uvxx { namespace fs {
         {
             mode |= std::ios_base::out;
             return file_buffer<_CharType>::open(file_name, mode, prot)
-                .then([](streams::streambuf<_CharType> buf) -> uvxx::streams::basic_ostream < _CharType >
+                .then([](streams::streambuf<_CharType> buf) -> uvxx::streams::basic_ostream<_CharType>
             {
                 return uvxx::streams::basic_ostream<_CharType>(buf);
             });
