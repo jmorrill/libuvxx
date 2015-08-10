0fa58124ed9bf2488d31ee8226e5654ec97d6dd2
Jeremiah Morrill <jeremiah.morrill@gmail.com>
2015-08-10 10:17:12 -0700

    test app changes

--- apps/tests/win_test_app/test_application.cpp
+++ apps/tests/win_test_app/test_application.cpp
@@ -27,11 +27,11 @@ void on_sample_callback(const media_sample& sample)
             printf("packet loss: %5.2f%%\n", stats.percent_packet_loss);
         }
 
-    /*    printf("codec: %s\t size: %d\t pts: %lld s:%u",
+        printf("codec: %s\t size: %d\t pts: %lld s:%u",
             sample.codec_name().c_str(),
             sample.size(),
             sample.presentation_time().count(),
-            sample.stream_number());*/
+            sample.stream_number());
 
         auto major_type = sample.attribute_get<sample_major_type>(ATTRIBUTE_SAMPLE_MAJOR_TYPE);
 
@@ -46,8 +46,7 @@ void on_sample_callback(const media_sample& sample)
 
             auto video_size = sample.attribute_get<video_dimensions>(ATTRIBUTE_VIDEO_DIMENSIONS);
 
-
-           // printf("\twxh: %dx%d", video_size.width, video_size.height);
+            printf("\twxh: %dx%d", video_size.width, video_size.height);
 
             if (key_frame)
             {
@@ -65,7 +64,7 @@ void on_sample_callback(const media_sample& sample)
             printf("\tchannels: %d", channels);
         }
 
-       // printf("\n");
+        printf("\n");
     }).then([=]
     {
         client.read_stream_sample();
@@ -78,17 +77,24 @@ void stream_closed(int stream_number)
 }
 
 
-server_media_session on_session_requested(const std::string& stream_name)
+task<server_media_session> on_session_requested(const std::string& stream_name)
 {
-    server_session = std::make_shared<server_media_session>();
+    server_session = nullptr;
+
+    auto session = std::make_shared<server_media_session>();
 
     media_descriptor descriptor;
 
     descriptor.add_stream_from_attributes(1, "H264", media_attributes());
     
-    server_session->set_media_descriptor(descriptor);
+    session->set_media_descriptor(descriptor);
 
-    return *server_session.get();
+    server_session = std::move(session);
+
+    return create_task([]
+    {
+        return *server_session.get();
+    });
 }
 
 int main(int argc, char* argv[])
@@ -113,7 +119,7 @@ int main(int argc, char* argv[])
 
         client.credentials_set("admin", "12345");
 
-        client.protocol_set(transport_protocol::tcp);
+        client.protocol_set(transport_protocol::udp);
 
         client.open(argv[1]).then([=]
         {
