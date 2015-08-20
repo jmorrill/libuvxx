64db8508ce87b710141468f303116a326b7a20b7
Jeremiah Morrill <jeremiah.morrill@gmail.com>
2015-08-20 00:26:14 -0700

    start of a dispatcher object base class

---
+++ libuvxx/include/event_dispatcher_object_base.hpp
@@ -0,0 +1,89 @@
+#pragma once
+
+#include "event_dispatcher_object.hpp"
+
+namespace uvxx
+{
+    template<typename INTERNAL_IMPL_T>
+    class event_dispatcher_object_base : public event_dispatcher_object
+    {
+    public:
+        event_dispatcher_object_base() :
+            /* In case the last reference of our class gets cleared by an foreign thread
+               we need to make sure it gets deleted on the correct dispatcher */
+            _container(std::shared_ptr<_private_impl_container>(new _private_impl_container,[=](_private_impl_container * container)
+        {
+            if (container->__impl && !check_access())
+            {
+                dispatcher().begin_invoke([impl = std::move(container->__impl)]() mutable
+                {
+                    impl = nullptr;
+                });
+            }
+
+            delete container;
+        }))
+        {
+        }
+
+        event_dispatcher_object_base(const event_dispatcher_object_base& rhs) = default;
+      
+        event_dispatcher_object_base& operator=(const event_dispatcher_object_base& rhs) = default;
+
+        event_dispatcher_object_base(event_dispatcher_object_base&& rhs)
+        {
+            *this = std::move(rhs);
+        }
+
+        event_dispatcher_object_base& operator=(event_dispatcher_object_base&& rhs)
+        {
+            _container = std::move(rhs->_container);
+
+            return *this;
+        }
+
+        bool operator=(std::nullptr_t rhs)
+        {
+            return _container->_impl = nullptr;
+        }
+
+        operator bool()
+        {
+            return (_container->_impl != nullptr);
+        }
+
+        bool operator==(std::nullptr_t rhs)
+        {
+            return (_container->_impl == nullptr);
+        }
+
+        bool operator !=(std::nullptr_t rhs)
+        {
+            return (_container->_impl != nullptr);
+        }
+
+        virtual ~event_dispatcher_object_base()
+        {
+           
+        }
+
+    protected:
+       void private_impl_set(const std::shared_ptr<INTERNAL_IMPL_T>& impl)
+       {
+           _container->__impl = impl;
+       }
+
+        std::shared_ptr<INTERNAL_IMPL_T>& private_impl()
+        {
+            return _container->__impl;
+        }
+
+    private:
+        struct _private_impl_container
+        {
+            std::shared_ptr<INTERNAL_IMPL_T> __impl;
+        };
+
+        std::shared_ptr<_private_impl_container> _container;
+    };
+}
